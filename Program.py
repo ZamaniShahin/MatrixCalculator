c3ecec0104c27e0f72cac1392d2a13fe61045312
ZamaniShahin <sh.zamani1581@gmail.com>
2023-08-24 16:08:28 +0330

    define substract_matrices()

--- Program.py
+++ Program.py
@@ -28,3 +28,12 @@ def add_matrices(matrix1, matrix2):
         result.append(row)
     
     return result
+def subtract_matrices(matrix1, matrix2):
+    result = []
+    for i in range(len(matrix1)):
+        row = []
+        for j in range(len(matrix1[0])):
+            row.append(matrix1[i][j] - matrix2[i][j])
+        result.append(row)
+    
+    return result
