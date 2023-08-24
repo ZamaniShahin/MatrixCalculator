0c207294a9b362841de2329209db6b0a9439acb8
ZamaniShahin <sh.zamani1581@gmail.com>
2023-08-25 00:20:28 +0330

    define multiply_matrices()

--- Program.py
+++ Program.py
@@ -37,3 +37,16 @@ def subtract_matrices(matrix1, matrix2):
         result.append(row)
     
     return result
+
+    def multiply_matrices(matrix1, matrix2):
+        result = []
+        for i in range(len(matrix1)):
+            row = []
+            for j in range(len(matrix2[0])):
+                element = 0
+                for k in range(len(matrix2)):
+                    element += matrix1[i][k] * matrix2[k][j]
+                row.append(element)
+            result.append(row)
+    
+    return result
