ea3329912cd98c664f45eb1cdbf97a65c9125041
ZamaniShahin <sh.zamani1581@gmail.com>
2023-08-25 00:26:48 +0330

    UI Part With while(true) loop done

--- Program.py
+++ Program.py
@@ -1,4 +1,6 @@
-
+# Define os lib and initial clear for clearing console.
+import os
+clear = lambda: os.system('cls')
 
 def take_matrix():
     rows = int(input("Enter the number of rows: "))
@@ -90,3 +92,53 @@ matrix2 = take_matrix()
 if(matrix2 == None):
     print('Your Given Matrix Is Not Compatible With 10*10 Maximum Matrix Length.')
     quit()
+
+#----------------------------------------------------------------------------------
+
+while(True):
+    print('*****************************************',end='\n')
+    flag = int(input('Enter Your Option : \n0.End Operation\n1.Addition.\n2.Substraction.\n3.Multiplicarion.\n4.Determination.\n5.Swap Matrices.\n6.Add A Matrix Into Other one.\n'))
+    clear()
+    if(flag ==0):
+        break
+    elif(flag == 1):
+        # Addition Tasks
+        if len(matrix1) == len(matrix2) and len(matrix1[0]) == len(matrix2[0]):
+            addition = add_matrices(matrix1, matrix2)
+            print("\nAddition of the matrices:")
+            print_matrix(addition)
+    elif(flag == 2):
+        #Substraction Tasks
+        if len(matrix1) == len(matrix2) and len(matrix1[0]) == len(matrix2[0]):
+            subtraction = subtract_matrices(matrix1, matrix2)
+            print("\nSubtraction of the matrices:")
+            print_matrix(subtraction)
+    elif(flag ==3):
+        #Multiplication Tasks
+        if len(matrix1[0]) == len(matrix2):
+            multiplication = multiply_matrices(matrix1, matrix2)
+            print("\nMultiplication of the matrices:")
+            print_matrix(multiplication)
+    elif(flag == 4):
+        #Determine Tasks
+        if len(matrix1) == len(matrix1[0]):
+            det1 = determine_matrix(matrix1)
+            det2 = determine_matrix(matrix2)
+            print("\nDeterminant of the first matrix:", det1)
+            print("Determinant of the second matrix:", det2)
+    elif(flag == 5):
+        #Swap Tasks
+        swapped_matrix1, swapped_matrix2 = swap_matrices(matrix1, matrix2)
+        print("\nSwapped matrices:")
+        print("Matrix 1:")
+        print_matrix(swapped_matrix1)
+        print("\nMatrix 2:")
+        print_matrix(swapped_matrix2)
+    elif(flag == 6):
+        # Add Matrix To Other One Tasks
+        if len(matrix1) == len(matrix2) and len(matrix1[0]) == len(matrix2[0]):
+            addition = add_matrix_into_another(matrix1, matrix2)
+            print("\nAdding one matrix into the other:")
+            print_matrix(addition)
+    else:
+        print('Please Enter An Option Between 1 to 6 .')
